25f06398b555645039861a7efa1bcecfa2af10ea
juliuswar <juliuswar@gmail.com>
2022-10-17 23:18:40 +0700

    update

--- home.php
+++ home.php
@@ -1,63 +1,106 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <title>Selamat Ulang Tahun ESTERRR !</title>
-    <style>        
+    <title>Selamat Ulang Tahun Oviee !</title>
+    <style>
         .text {
             animation: change 10s step-end both;
         }
 
-        @keyframes change {
-            0%      {background:white;}
-            25%     {background:lawngreen;}
-            75%     {background:plum;}
-            100%    {background:burlywood;}
+        <blade keyframes|%20change%20%7B>0% {
+            background: white;
         }
+
+        25% {
+            background: lawngreen;
+        }
+
+        75% {
+            background: plum;
+        }
+
+        100% {
+            background: burlywood;
+        }
+        }
+
         .text2 {
             animation: change 10s step-end both;
         }
 
-        @keyframes change {
-            0%      {background:burlywood;}
-            25%     {background:lawngreen;}
-            75%     {background:plum;}
-            100%    {background:white;}
+        <blade keyframes|%20change%20%7B>0% {
+            background: burlywood;
+        }
+
+        25% {
+            background: lawngreen;
+        }
+
+        75% {
+            background: plum;
+        }
+
+        100% {
+            background: white;
+        }
         }
     </style>
-  </head>
-  <body style=" background-image: url('2.jpg'); ">
+</head>
+
+<body style=" background-image: url('2.jpg'); ">
+    <iframe src="song.mp3" allow="autoplay" id="audio" style="display:none"></iframe>
+    <audio id="player" autoplay loop>
+        <source src="song.mp3" type="audio/mp3">
+    </audio>
     <center>
         <div style="padding-top: 2%;">
-            <h1 class="w3-container w3-center w3-animate-top" style="padding-top:1%; font-family:Lucida Handwriting; background-color:yellow; width:50%;">Selamat Ulang Tahun</h1>
+            <h1 class="w3-container w3-center w3-animate-top"
+                style="padding-top:1%; font-family:Lucida Handwriting; background-color:yellow; width:50%;">Selamat
+                Ulang Tahun</h1>
         </div>
-        <h2 class="w3-center w3-animate-bottom" style="padding-top:1%; font-family:Lucida Console; background-color:cyan; width:50%;">ESTERRR</h2>
-        
-        <h2 class="text" style="margin-top:3%; font-family:Lucida Console; background-color:gold; width:80%;">Yang Ulang Tahunnya Setelah Malam Natal dan <br>Sebelum Malam Tahun Baru :></h3>
-       
-        <img src="pika.gif" style="width: 10%;height:10%; border: 10px solid blue; border-radius: 50px 50px;  margin-top:2%">
-        <div  style="margin-top: 3%;">     
+        <h2 class="w3-center w3-animate-bottom"
+            style="padding-top:1%; font-family:Lucida Console; background-color:cyan; width:50%;">OVIEE</h2>
+
+        <h2 class="text" style="margin-top:3%; font-family:Lucida Console; background-color:gold; width:80%;">Maaf ya
+            ngucapin nya telatt (｡•́︿•̀｡)</h2>
+
+        <img src="pika.gif"
+            style="width: 10%;height:10%; border: 10px solid blue; border-radius: 50px 50px;  margin-top:2%">
+        <div style="margin-top: 3%;">
             <img src="lucu.gif" style="width: 10%;height:10%; border: 10px solid blue; border-radius: 50px 50px;">
-            
-            <img class="w3-container w3-center w3-animate-zoom" src="ester.jpg" style="margin-left:3%;margin-bottom:3%;margin-right:3%;width: 40%;height:50%; border: 10px solid red; border-radius: 50px 50px;">
-           
+
+            <img class="w3-container w3-center w3-animate-zoom" src="ovie.png"
+                style="margin-left:3%;margin-bottom:3%;margin-right:3%;width: 40%;height:50%; border: 10px solid red; border-radius: 50px 50px;">
+
             <img src="lucu2.gif" style="width: 10%;height:10%; border: 10px solid blue; border-radius: 50px 50px;">
         </div>
         <img src="lucu3.gif" style="width: 10%;height:10%; border: 10px solid blue; border-radius: 50px 50px;">
 
-        <h3 style="margin-left:2%;margin-top:5%; font-family:Lucida Console; background-color:darksalmon; width:80%;">Semoga Panjang Umur dan Sehat selalu yaaa...</h3>
-       
-        <h3 class="text2" style="margin-left:2%;margin-top:4%; font-family:Lucida Console; background-color:white; width:80%;">Pesanku: Jangan Menyerah gapai impian yaa. Soalnya Impian besarmu harus diwujudkan.</h3>
-        <h3 class="text2" style="margin-left:2%;margin-top:1%; font-family:Lucida Console; background-color:white; width:80%;">Buatlah bangga orang - orang yang sudah mendoakan.</h3>
-        
+        <h3 style="margin-left:2%;margin-top:5%; font-family:Lucida Console; background-color:darksalmon; width:80%;">
+            Semoga Panjang Umur dan Sehat selalu yaaa...</h3>
+
+        <h3 class="text2"
+            style="margin-left:2%;margin-top:4%; font-family:Lucida Console; background-color:white; width:80%;">
+            Pesanku: Jangan menyerah apapun persoalannya. Andalkan Tuhan trus （＾ｖ＾）</h3>
+        <h3 class="text2"
+            style="margin-left:2%;margin-top:1%; font-family:Lucida Console; background-color:white; width:80%;">Semoga
+            cepet selesai ya skripsinya. Semangatt (^^)</h3>
+
         <div class="class" style="margin-bottom: 5%;"></div>
     </center>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
+    </script>
+
+</body>
+
 </html>
